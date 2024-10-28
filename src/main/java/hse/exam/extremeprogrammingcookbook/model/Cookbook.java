e9a48df91baaaaa5541ccfc24e83b80a11529833
Igor <greafdorothy@yandex.com>
2024-10-28 12:15:02 +0300

    add recipe and cookbook templates

---
+++ src/main/java/hse/exam/extremeprogrammingcookbook/model/Cookbook.java
@@ -0,0 +1,20 @@
+package hse.exam.extremeprogrammingcookbook.model;
+
+public class Cookbook {
+
+    Recipe getRecipe(String name) {
+        return null;
+    }
+
+    void addRecipe(Recipe recipe) {
+
+    }
+    private void loadIngredients(String filepath) {
+
+    }
+
+    private void loadRecipe(String filepath) {
+
+    }
+
+}
---
+++ src/main/java/hse/exam/extremeprogrammingcookbook/model/Recipe.java
@@ -0,0 +1,27 @@
+package hse.exam.extremeprogrammingcookbook.model;
+
+import javafx.util.Pair;
+
+import java.util.List;
+
+public class Recipe {
+    private static List<String> ingredients;
+
+    Recipe() {
+
+    }
+
+    boolean addIngredient(String ingredient) {
+        return false;
+    }
+
+    boolean removeIngredient(String ingredient) {
+        return false;
+    }
+
+    List<Pair<String, Integer>> getIngredientList() {
+        return null;
+    }
+
+
+}
